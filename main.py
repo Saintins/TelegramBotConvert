9dbffdcc14dea7fecb2ed81499bf67c51470141a
Alex Necheporenko <al.necheporenko@gmail.com>
2023-06-13 20:13:57 +0300

    add Account

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ bot = telebot.TeleBot('6005437532:AAFgaZpYzH83a8_NqJJCFpSIks8tscELsvI')
 @bot.message_handler(commands=['start'])
 # hi
 def start(message):
-    bot.send_message(message.chat.id, '<b> Привет :)</b>', parse_mode='html')
+    mess = f"Привет, <b>{message.from_user.first_name}{message.from_user.last_name}</b>"
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
 
 
 bot.polling(none_stop=True)
